2df6e2c874fbb1ba6a91239b7d00b41d2610de29
Tommy O'Keefe <okeefe@teralogics.com>
2021-10-26 12:27:06 -0400

    first commit KDCMusic

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,31 +1,38 @@
 module.exports = {
-  siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "Kyle David Crosby",
-  },
-  plugins: [
-    "gatsby-plugin-netlify-cms",
-    "gatsby-plugin-styled-components",
-    "gatsby-plugin-image",
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-mdx",
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "images",
-        path: "./src/images/",
-      },
-      __key: "images",
-    },
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "pages",
-        path: "./src/pages/",
-      },
-      __key: "pages",
-    },
-  ],
+	siteMetadata: {
+		siteUrl: "https://www.yourdomain.tld",
+		title: "Kyle David Crosby",
+	},
+	plugins: [
+		"gatsby-plugin-netlify-cms",
+		"gatsby-plugin-styled-components",
+		"gatsby-plugin-image",
+		"gatsby-plugin-react-helmet",
+		"gatsby-plugin-mdx",
+		"gatsby-plugin-sharp",
+		"gatsby-transformer-sharp",
+		{
+			resolve: "gatsby-source-filesystem",
+			options: {
+				name: "images",
+				path: "./src/images/",
+			},
+			__key: "images",
+		},
+		{
+			resolve: "gatsby-source-filesystem",
+			options: {
+				name: "pages",
+				path: "./src/pages/",
+			},
+			__key: "pages",
+		},
+		{
+			resolve: `gatsby-plugin-google-fonts`,
+			options: {
+				fonts: [`Lato\:300,400,800,900`],
+				display: "swap",
+			},
+		},
+	],
 };
---
+++ src/components/heroImage.js
@@ -0,0 +1,11 @@
+import React from "react";
+import styled from "styled-components";
+
+const HeroImage = styled.img`
+	display: block;
+	max-width: 80%;
+	margin: 50px 10%;
+	box-shadow: 5px 5px 5px rgb(118, 60, 57);
+`;
+
+export default HeroImage;
